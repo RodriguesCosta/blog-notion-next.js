3b84e720f9a500b1430b24535ce88668f2c40225
Rodrigues Costa <rodriguescosta@rodriguescosta.com>
2021-06-16 17:12:42 -0300

    feat: basic config

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { NextPage } from 'next';
+
+const PageComponent: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default PageComponent;
