3b2e51c186de3dd0bdfce78c93a956e97532f8f6
Rodrigues Costa <rodriguescosta@rodriguescosta.com>
2021-06-21 17:44:37 -0300

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Text } from '@chakra-ui/react';
+
+export const Header: React.FC = () => {
+  return <Text>Header</Text>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import { Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { NextPage } from 'next';
+
+import { Header } from '~/components/Header';
 
 const PageComponent: NextPage = () => {
   return (
-    <div>
-      <Text>Hello</Text>
-    </div>
+    <Flex direction="column">
+      <Header />
+
+      <Flex>
+        <Text>Home</Text>
+      </Flex>
+    </Flex>
   );
 };
 
